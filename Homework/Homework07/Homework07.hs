5308fdea3f312a424630ea0abea81ddfefe81561
Steo <palutz@users.noreply.github.com>
2022-11-29 15:40:27 +0000

    homework 07

--- Homework/Homework07/Homework07.hs
+++ Homework/Homework07/Homework07.hs
@@ -1,32 +1,32 @@
 -- Question 1
 -- Investigate the `Bounded` type class. What behaviours it provides?
-minBound :: a
-maxBound :: a
+--  minBound :: a
+--  maxBound :: a
 
 
 -- Question 2
 -- The types Int and Word bellong to the same type classes. What is the difference
 -- between them? Check maybe the maxBound and minBound parameter for both types.
-maxBound :: Int
-9223372036854775807
-maxBound :: Word
-18446744073709551615
-minBound :: Int
--9223372036854775808
-minBound :: Word
-0 
+--   maxBound :: Int
+--   9223372036854775807
+--   maxBound :: Word
+--   18446744073709551615
+--   minBound :: Int
+--   -9223372036854775808
+--   minBound :: Word
+--   0 
 
 -- Question 3
 -- Investigate the `Enum` type class. What behaviours provides?
-  succ :: a -> a
-  pred :: a -> a
-  toEnum :: Int -> a
-  fromEnum :: a -> Int
-  enumFrom :: a -> [a]
-  enumFromThen :: a -> a -> [a]
-  enumFromTo :: a -> a -> [a]
-  enumFromThenTo :: a -> a -> a -> [a]
-  {-# MINIMAL toEnum, fromEnum #-}
+--   succ :: a -> a
+--   pred :: a -> a
+--   toEnum :: Int -> a
+--   fromEnum :: a -> Int
+--   enumFrom :: a -> [a]
+--   enumFromThen :: a -> a -> [a]
+--   enumFromTo :: a -> a -> [a]
+--   enumFromThenTo :: a -> a -> a -> [a]
+--   {-# MINIMAL toEnum, fromEnum #-}
 
 
 -- Question 4
@@ -38,13 +38,16 @@ f1 x y z = show (x / y) ++ z
 f2 :: (Bounded a, Enum a, Eq a) => a -> a
 f2 x = if x == maxBound then minBound else succ x
 
+f2' :: (Bounded a, Enum a, Eq a) => a -> a
+f2' x = if x == maxBound then minBound else succ x
+
 -- Question 5
 -- Investigate the numeric type classes to figure out which behaviors they provide to change between numeric types.
-Fractional 
-    fromRational :: Rational -> a
-Real 
-    toRational :: a -> Rational
-Integer
-    fromInteger :: Integer -> a
-Integral
-    toInteger :: a -> Integer
+--  Fractional 
+--      fromRational :: Rational -> a
+--  Real 
+--      toRational :: a -> Rational
+--  Integer
+--      fromInteger :: Integer -> a
+--  Integral
+--      toInteger :: a -> Integer
